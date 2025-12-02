1bc869eafc7fc0f225a00005bf721dfd511b2170
HuaLiMao-AQ <hualimao_aq@163.com>
2025-12-02 10:39:47 +0800

    R11: Import from oppo to audio_policy_configuration.xml
    Signed-off-by: HuaLiMao-AQ <hualimao_aq@163.com>

--- device.mk
+++ device.mk
@@ -12,6 +12,8 @@ $(call inherit-product, vendor/oppo/R11/R11-vendor.mk)
 
 # Audio configs
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
+    $(LOCAL_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_ODM)/etc/audio_policy_configuration.xml \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/audio_platform_info_intcodec.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info_intcodec.xml \
     $(LOCAL_PATH)/audio/mixer_paths_mtp.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_mtp.xml
